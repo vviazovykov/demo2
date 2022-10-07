81c83517d757b5488cf0c798b69c2196a2ccd6fd
Vasyl <vasyl.viazovykov@gmail.com>
2022-10-07 14:04:18 +0300

    Fix table creation

--- database/sql/create_tables.sql
+++ database/sql/create_tables.sql
@@ -1,17 +1,17 @@
-create table "user" (
+create table IF NOT EXISTS person (
                         id varchar(255) not null,
                         name varchar(50) not null,
                         primary key (id)
 );
 
-create table exam (
+create table IF NOT EXISTS exam (
                       id bigserial not null,
                       title varchar(50) not null,
                       description varchar(512) not null,
                       primary key (id)
 );
 
-create table question (
+create table IF NOT EXISTS question (
                           id bigserial not null,
                           exam_id bigint not null references exam (id),
                           question_order bigint not null,
@@ -19,7 +19,7 @@ create table question (
                           primary key (id)
 );
 
-create table alternative (
+create table IF NOT EXISTS alternative (
                              id bigserial not null,
                              question_id bigint not null references question (id),
                              alternative_order bigint not null,
@@ -28,9 +28,9 @@ create table alternative (
                              primary key (id)
 );
 
-create table attempt (
+create table IF NOT EXISTS attempt (
                          id bigserial not null,
-                         user_id varchar(255) not null references "user" (id),
+                         user_id varchar(255) not null references person (id),
                          alternative_id bigint not null references alternative (id),
                          correct boolean not null,
                          date timestamp without time zone not null,
